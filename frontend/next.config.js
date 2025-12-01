0de13615331c3d3b7ef995003d4fb7ab310e4bcb
muhammadh03 <muhammadhjalaldeen03@gmail.com>
2025-12-01 22:47:11 +0530

    another small change

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'images.pexels.com'],
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
